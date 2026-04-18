ef952e6c09d963f1df30b353820fa0c1b904207c
afrid <abujarafrid@gmail.com>
2026-04-18 22:54:38 +0600

    first commit

---
+++ InventoryManagementSystem/Forms/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InventoryManagementSystem.Forms.Product;
+
+namespace InventoryManagementSystem.Forms
+{
+    public partial class MainForm: Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProductForm_Click(object sender, EventArgs e)
+        {
+           AddProductForm pForm = new AddProductForm();
+            pForm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ InventoryManagementSystem/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InventoryManagementSystem.Forms;
+using InventoryManagementSystem.Forms.Product;
+
+namespace InventoryManagementSystem
+{
+    public partial class Login: Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string userName = txtUserName.Text;
+            string password = txtPassword.Text;
+
+            //Check
+            if (userName == "admin" && password == "admin") 
+            {
+                MainForm mForm = new MainForm();
+                mForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("User name or password incorrect");
+            }
+
+            
+        }
+    }
+}
